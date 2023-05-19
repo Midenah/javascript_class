5d7a56ddf191e33c93aeb31377cbeff5b3c0d125
oluyomi justina <justinaolamide2611@gmail.com>
2023-05-19 20:18:19 +0100

    fix

---
+++ classTwo.js
@@ -0,0 +1,51 @@
+const male = 9
+const female = 2
+
+const gender = {
+    male,
+    female,
+}
+
+const colors = [
+   "black",
+    "white",
+    "grey",
+    "ankara",
+    "navy",
+    "green",[]
+]
+
+const one = "string";
+const two = 1;
+const three = {
+    obj:"2"
+}
+
+const four = true;
+
+console.log (typeof gender,  
+    typeof colors,
+    typeof one,
+    typeof two,
+    typeof three,
+    typeof four);
+
+    let add = male + female;
+    console.log(add--)
+    let sub = male - female;
+    console.log(sub++)
+    let divide = male / female;
+    let multi = male * female;
+    let reminder = male % female;
+
+let mult = 2 * 4;
+let mult2 = 2 ** 4;
+console.log(mult, mult2)
+
+
+   console.log (add,
+         sub, 
+         divide, 
+         multi, 
+         reminder,
+        )
