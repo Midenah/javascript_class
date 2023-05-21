a3552470dd2db2ec8e7ac7d82bbd8dd32efd67e9
oluyomi justina <justinaolamide2611@gmail.com>
2023-05-21 11:58:27 +0100

    fix

---
+++ functions.js
@@ -0,0 +1,24 @@
+// function fullName (){
+//     let firstName = "Justina"
+//     let middleName = "Olamide"
+//     let lastName = "Oluyomi"
+//     let names = firstName + middleName + lastName;
+//     return names
+// }
+// console.log (fullName());
+
+
+// function addTwoNums (a,b){
+//     let alpha = a + b;
+// if (alpha > 10){
+//     return(`${alpha} More than a decade`)
+// }else{
+//     return(`${alpha}less than a decade`)
+// }
+// }
+// console.log(addTwoNums(10,78))
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
