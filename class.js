f3166581010e744b2ee33bff28fd73e71eb17103
oluyomi justina <justinaolamide2611@gmail.com>
2023-05-22 16:28:32 +0100

    fd

---
+++ class.js
@@ -0,0 +1,8 @@
+// function Humans (){
+//     this.family = "Vertebrae"
+//     this.limbs  = 2
+//     this.type = "Higher Mammals"
+//  }
+
+//  let jay = new Humans();
+//  jay.limbs
--- functions.js
+++ functions.js
@@ -18,7 +18,3 @@
 // }
 // console.log(addTwoNums(10,78))
 
-let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < 10; i++) {
-    console.log(i)
-}
