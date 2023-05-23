34ee4631dd0a5c2e091fc23e4577471143c420f8
oluyomi justina <justinaolamide2611@gmail.com>
2023-05-23 14:22:21 +0100

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,16 +1,75 @@
-const people =[]
-people.push("evan")
-people.push ("kelechi")
-people.push ("nduka")
-people.push ("isreal") 
-people.push ("kelvin") 
-people.push ("justina")
-people.push ("dolapo") 
-people.push ("ifeayin")
-people.push ("folarin")
-people.push("julius")
-console.log(people)
-people.pop()
-console.log(people)
-people.shift()
-console.log(people);
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+//1
+// //for loop
+// for (let i = 0; i < countries.length; i++){
+//   console.log(i - 1)
+// }
+
+// //while
+// let p = 0
+// while(p < countries.length){
+//   console.log(p - 1)
+//   p++;
+// }
+
+// //Do while
+// let n = 0;
+// do {
+//   console.log(n - 1);
+//   n++;
+// } while(n < countries.length);
+
+//2
+// // for
+// for (let i = countries.length; i >= 0; i--) {
+//   console.log(i - 1)
+// }
+
+// //while
+// let p = countries.length; 
+// while(p > 0) {
+//   console.log(p - 1);
+//   p--;
+// }
+
+// //do while
+// let n = countries.length;
+// do{
+//   console.log(n -1)
+//   n--;
+// }while(n > 0);
+
+// //3
+// for (let i = 0; i < n; i++){
+//   console.log(i)
+// }
+
+//4
+// for (let i = 1; i < p; i++){
+//   let
+// }
