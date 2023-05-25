c9c718348633e8456bafce55e5449b4dcd1815f2
oluyomi justina <justinaolamide2611@gmail.com>
2023-05-25 22:47:18 +0100

    fix

--- loops.js
+++ loops.js
@@ -73,3 +73,70 @@ const mernStack = ["MongoDB", "Express", "React", "Node"];
 // for (let i = 1; i < p; i++){
 //   let
 // }
+
+// // 5
+// for(let i = 0; i <= 10; i++){
+//   let n = i*i
+//   console.log(`${i} * ${i} = ${n}`)
+// }
+
+// //6
+// for(let i= 0; i <= 10; i++){
+//   let sub = i*i
+//   let num = sub * i
+//   console.log(`${i} ${sub} ${num}`)
+// }
+
+// //7
+// for(let i =0; i<=10; i++){
+//   if(i % 2 == 0){
+//     console.log(i)
+//   }
+// }
+
+// //8
+// for(let i = 0; i<=100; i++){
+//   if(i % 2 !== 0){
+//     console.log(i)
+//   }
+// }
+
+//9
+
+// //10
+// let sum = 0
+// for(let i = 0; i<= 100; i++){
+//   sum = sum + i;
+// }console.log('The sum of all numbers from 0 to 100 is '+ sum)
+
+// //11
+// let even = 0;
+// let odd = 0; 
+// for(let i = 0; i <= 100; i++){
+//   if (i % 2 == 0)
+//   even = even + i;
+// }
+// for (let n = 0; n <= 100; n++){
+//   if (n % 2 !== 0)
+//   odd = odd + n;
+// };
+// console.log(`The sum of all evens from 0 to 100 is ${even} . And the sum of all odds from 0 to 100 is ${odd}`)
+
+// //12
+// let even = 0;
+// let odd = 0; 
+// for(let i = 0; i <= 100; i++){
+//   if (i % 2 == 0)
+//   even = even + i;
+// }
+// for (let n = 0; n <= 100; n++){
+//   if (n % 2 !== 0)
+//   odd = odd + n;
+// };
+// console.log([even, odd])
+
+//13
+//14
+//15
+
+//level 2
