f7de23de9ca5b581102d816bcb04fdc4cc05d2fa
oluyomi justina <justinaolamide2611@gmail.com>
2023-06-02 15:29:08 +0100

    fix

--- dom.js
+++ dom.js
@@ -10,25 +10,82 @@
 // console.log (titles[3].id);
 // }
 
-const cret = document.createElement("div")
-const cret1 = document.createElement("span")
- const container = document.getElementById('container');
- const pi = document.querySelector("p");
- container.appendChild(cret)
- cret.appendChild(cret1)
- cret1.innerText = "February Cohort";
- pi.classList.add("bold_text");
+// const cret = document.createElement("div")
+// const cret1 = document.createElement("span")
+//  const container = document.getElementById('container');
+//  const pi = document.querySelector("p");
+//  container.appendChild(cret)
+//  cret.appendChild(cret1)
+//  cret1.innerText = "February Cohort";
+//  pi.classList.add("bold_text");
 
- 
-// let user = prompt('Enter');
-// user = parseInt() 
-// switch(user){
-//     case user = 18:
-//     console.log("you can drive");
-//     break;
-//     case user > 18:
-//     console.log("oga go sit down");
-//     break;
-//     default:
-//         console.log("you cant drive");
-// }
+// const dom1 = document.querySelector("p")
+// console.log(dom1)
+
+// const dom1 = document.getElementById("one")
+// console.log(dom1)
+// const dom2 = document.getElementById("two")
+// console.log(dom2)
+// const dom3 = document.getElementById("three")
+// console.log(dom3)
+// const dom4 = document.getElementById("four")
+// console.log(dom4)
+
+// //3
+// const domall = document.querySelectorAll("p")
+// console.log(domall)
+
+// //4
+// const text = document.querySelectorAll("p")
+// for(let i = 0; i < text;){
+//     console.log(t)
+
+
+// //5
+// const text4 = document.getElementById("four")
+// text4.textContent="Fourth paragragh";
+// console.log(text4.textContent)
+
+
+// //6
+// const para = document.querySelector("p");
+// para.id ="first";
+// const textnode = document.createTextNode("Water");
+// para.appendChild(textnode)
+
+// const two = document.getElementById('two');
+// two.className = "second";
+
+// const three = document.getElementById("three");
+// three.id = "third";
+
+// const four = document.getElementsById("four");
+// four.className = "fourth"
+
+// //level 2
+// const plus = document.querySelector("p");
+// plus.style.color="red";
+// plus.classList.add("text_p")
+
+// const add = document.getElementById("two");
+// add.classList.add("color_text");
+
+// //2
+// let allPara = document.querySelectorAll("p");
+// for (let i=0; i < allPara.length; i++){
+//     allPara = allPara[i];
+//     if (i==0 || i==2){
+//         allPara.style.color="green";
+//         }else if (i==1 || i==2){
+//             allPara.style.color="red";
+//         }
+//     }     
+
+// // 3
+// let paragragh = document.querySelector("p");
+// paragragh.id='ok'
+// paragragh.innerHTML="wow"
+
+// let para = document.getElementById("two");
+// para.id='ok'
+// para.innerHTML="wow"
